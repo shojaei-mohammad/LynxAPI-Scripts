fb8a1c614ecd6cb12626428295692e681066086c
mamad <mhmdshojaee@gmail.com>
2023-11-07 09:16:46 +0330

    export the python path before running the session.py

--- lynxapi.sh
+++ lynxapi.sh
@@ -49,28 +49,24 @@ fi
 
 # Step 1: Clone the FastAPI app repository
 colored_echo "Cloning the repository..."
-git clone $REPO_URL $SOURC_DIR
+git clone $REPO_URL "$SOURC_DIR"
 
 # Step 2: Create a virtual environment in the main directory
 colored_echo "Creating a virtual environment..."
-virtualenv -p python3 $MAIN_DIR/venv
+virtualenv -p python3 "$MAIN_DIR"/venv
 
 # Activate the virtual environment
-source $MAIN_DIR/venv/bin/activate
+source "$MAIN_DIR"/venv/bin/activate
 
-cd $SOURC_DIR
+cd "$SOURC_DIR"
 
 # Step 3: Install the requirements in the virtual environment
 colored_echo "Installing requirements in the virtual environment..."
 pip install -r ./requirements.txt
 
-# Step 3: Initialize the database directory
+# Step 4: Initialize the database directory
 colored_echo "Creating the database directory..."
-mkdir -p $DATABASE_DIR
-
-# Step 4: Run session.py to initialize the database
-colored_echo "Initializing the database..."
-python3 app/db/session.py
+mkdir -p "$DATABASE_DIR"
 
 # Step 5: Generate a secure API secret (using openssl for generating a secure random string)
 colored_echo "Generating a secure API secret..."
@@ -78,21 +74,26 @@ API_SECRET_VALUE=$(openssl rand -hex 32)
 
 # Step 6: Replace the API secret in the .env file, update paths, and read Uvicorn settings
 colored_echo "Configuring environment variables..."
-mv $ENV_SAMPLE_FILE $ENV_FILE
+mv "$ENV_SAMPLE_FILE" "$ENV_FILE"
 
-sed -i "s|API_SECRET_KEY=your_secret_key_here|API_SECRET_KEY=$API_SECRET_VALUE|" $ENV_FILE
-sed -i "s|SCRIPTS_PATH=/path/to/app/scripts/network-config.sh|SCRIPTS_PATH=$NETWORK_CONFIG_SCRIPT|" $ENV_FILE
-sed -i "s|SQLALCHEMY_DATABASE_URL=\"sqlite:///path/to/db.sqlite3\"|SQLALCHEMY_DATABASE_URL=\"$DATABASE_URL\"|" $ENV_FILE
+sed -i "s|API_SECRET_KEY=your_secret_key_here|API_SECRET_KEY=$API_SECRET_VALUE|" "$ENV_FILE"
+sed -i "s|SCRIPTS_PATH=/path/to/app/scripts/network-config.sh|SCRIPTS_PATH=$NETWORK_CONFIG_SCRIPT|" "$ENV_FILE"
+sed -i "s|SQLALCHEMY_DATABASE_URL=\"sqlite:///path/to/db.sqlite3\"|SQLALCHEMY_DATABASE_URL=\"$DATABASE_URL\"|" "$ENV_FILE"
 
 # Read Uvicorn settings from .env
-UVICORN_HOST=$(grep UVICORN_HOST $ENV_FILE | cut -d '=' -f2)
-UVICORN_PORT=$(grep UVICORN_PORT $ENV_FILE | cut -d '=' -f2)
+UVICORN_HOST=$(grep UVICORN_HOST "$ENV_FILE" | cut -d '=' -f2)
+UVICORN_PORT=$(grep UVICORN_PORT "$ENV_FILE" | cut -d '=' -f2)
 
-# Step 7: Create an API user
+# Step 7: Run session.py to initialize the database
+colored_echo "Initializing the database..."
+export PYTHONPATH="${PYTHONPATH}:$SOURC_DIR"
+python3 app/db/session.py
+
+# Step 8: Create an API user
 colored_echo "Creating an API user..."
-python3 $CREATE_USER_SCRIPT
+python3 "$CREATE_USER_SCRIPT"
 
-# Step 8: Run the FastAPI app using Uvicorn with settings from the .env file
+# Step 9: Run the FastAPI app using Uvicorn with settings from the .env file
 colored_echo "Starting the FastAPI app using Uvicorn..."
 nohup uvicorn app.main:app --host "$UVICORN_HOST" --port "$UVICORN_PORT" &
 
